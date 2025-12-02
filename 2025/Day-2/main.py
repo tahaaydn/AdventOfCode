a5206e31ee7159e1bad2b7a0905b53b841055924
tahaaydn <tahaaydn@gmail.com>
2025-12-02 19:59:03 +0300

    AoC-Day2-Part2

--- 2025/Day-2/main.py
+++ 2025/Day-2/main.py
@@ -1,34 +1,30 @@
 def read_input():
-    with open("input.txt") as file:
-        input = file.read()
-    
-    return input.split(",")
+    with open("test.txt") as file:
+        input = file.read().split(",")
+    return input
 
 def process_ranges(id_ranges):
     invalid_numbers = []
     for id_range in id_ranges:
         start, end = map(int, id_range.split("-"))
-        print(f"Start: {start}, End: {end}")
         for num in range(start, end + 1):
-            length = int(len(str(num)))
-            if length % 2 == 0:
-                e = length / 2
-                q = num // 10 ** e
-                r = num % 10 ** e
-                if q == r:
-                    invalid_numbers.append(num)
+            length = len(str(num))
 
-        print(f"Invalid Numbers so far: {invalid_numbers}")
+            for i in range(1, length//2 + 1):
+                if length % i != 0:
+                    continue
+                r = length // i
+
+                q = sum((10**(i*j) for j in range(0, r)))
+                if num % q == 0:
+                    invalid_numbers.append(num)
+                    print(f"Checking {num} with q={q}, length={length}, i={i}, r={r}, length//2={length//2}")
+                    break
     return invalid_numbers
 
 # ---
 
 id_ranges = read_input()
 invalid_numbers = process_ranges(id_ranges)
+
 print(f"Total Invalid Numbers: {sum(invalid_numbers)}")
-
-# print(id_ranges)
-# print(f"Invalid Numbers: {invalid_numbers}")
-
-# string = "1010"
-# print(string[0:2])
